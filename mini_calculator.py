1a3b1228da4b903b3bb0396986b3a8fb75f794a5
Bala ganesh V <balavijayan03@gmail.com>
2026-08-18 09:57:14 +0530

    Add files via upload

---
+++ mini_calculator.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the number A :"))
+b=int(input("Enter the number B :"))
+operation=input("add/sub/mul/div :")
+if(operation=="add"):
+    print(a+b)
+elif(operation=="sub"):
+    print(a-b)
+elif(operation=="mul"):
+    print(a*b)
+elif(operation=="div"):
+    print(a/b)
